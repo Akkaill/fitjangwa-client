1e12e1bfd221658d1153e3e8e4c4c21260633661
Akkaill <Kunlaphat_@hotmail.com>
2023-11-08 21:42:23 +0700

    add api

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { useRouter } from "next/navigation";
 import { CoureseLists } from "./CoureseList";
+import Link from "next/link";
 import {
   Card,
   CardContent,
@@ -13,30 +14,53 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-const ProductPage = () => {
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  desc: string;
+  images: string[];
+  stock: number;
+  detail: string;
+  slug: string;
+  createdAt: Date;
+  updatedAt: Date;
+  category: Category[];
+}
+
+export interface Category {
+  id: string;
+  name: string;
+  slug: string;
+}
+
+function ProductPage({ product }: { product: Product }) {
   const { push } = useRouter();
+
   return (
-    <div>
-      <div className="container grid grid-cols-4 gap-4">
-        {CoureseLists.map((m) => (
-          <Card key={m.id} className="h-auto">
-            <CardHeader>
-              <CardTitle className="text-xl font-semibold font-serif">
-                {m.title}
-              </CardTitle>
-              <CardDescription>{m.desc}</CardDescription>
-            </CardHeader>
-            <CardContent className="w-max h-40">
-              <Image src={m.href} alt="pic" width={250} height={300}></Image>
-            </CardContent>
-            <CardFooter className="p-20">
-              <Button onClick={() => push(m.link)}>สนใจดูรายละเอียด</Button>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
-    </div>
+    <Card className="h-auto">
+      <CardHeader>
+        <CardTitle className="text-xl font-semibold font-serif">
+          {product.name}
+        </CardTitle>
+        <CardDescription>{product.desc}</CardDescription>
+      </CardHeader>
+      <CardContent className="w-max h-40">
+        <Image
+          src={product.images[0]}
+          alt="pic"
+          width={250}
+          height={300}
+          style={{ objectFit: "contain" }}
+        ></Image>
+      </CardContent>
+      <CardFooter className="p-20">
+        <Link href={`/products/${product.slug}`} className={buttonVariants()}>
+          สนใจดูรายละเอียด
+        </Link>
+      </CardFooter>
+    </Card>
   );
-};
+}
 
 export default ProductPage;
--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -3,14 +3,40 @@ import React from "react";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import { THB } from "@/lib/format";
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  desc: string;
+  images: string[];
+  stock: number;
+  detail: string;
+  slug: string;
+  createdAt: Date;
+  updatedAt: Date;
+  category: Category[];
+}
 
-const ProductDeatial = () => {
+export interface Category {
+  id: string;
+  name: string;
+  slug: string;
+}
+
+const ProductDeatail = ({ product }: { product: Product }) => {
   const { push } = useRouter();
   return (
     <div className="container grid grid-cols-3 gap-5  max-h-screen">
       <div className="col-span-1  border-slate-300 border-solid border max-h-max h-120 p-3">
         <div className="h-full">
-          <Image src="/cbum.png" alt="logo" width={560} height={560}></Image>
+          <Image
+            src={product.images[0]}
+            alt="logo"
+            width={560}
+            height={560}
+            style={{ objectFit: "contain" }}
+          ></Image>
         </div>
       </div>
       <div className="col-span-2 m-2 px-10">
@@ -20,7 +46,7 @@ const ProductDeatial = () => {
         <p className=" text-lg pb-2">
           โดยจะประกอบไปด้วยการกายภาพที่สามารถทำได้ที่บ้านเเละฟิตเนส
         </p>
-        <p className="text-blue-500 pb-20">1,299 Baht</p>
+        <p className="text-blue-500 pb-20">{THB(product.price)}</p>
         <Button className="w-full" onClick={() => push("/")}>
           กดเพื่อสั่งซื้อ
         </Button>
@@ -29,4 +55,4 @@ const ProductDeatial = () => {
   );
 };
 
-export default ProductDeatial;
+export default ProductDeatail;
